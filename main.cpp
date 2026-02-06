502bc5dfc46ae02141a588a7ded7184e0e60bec1
rockcutter <rockcutter414@gmail.com>
2026-02-07 01:11:16 +0900

    add log

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "httplib.h"
+#include "spdlog/spdlog.h"
 
 int main() {
   httplib::Server svr;
 
   svr.Get("/ping", [](const httplib::Request& req, httplib::Response& res) {
+    spdlog::info("Received ping");
     res.set_content("pong", "text/plain");
   });
 
